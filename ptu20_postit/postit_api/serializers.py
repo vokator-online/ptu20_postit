4b552125031e2c36a3a8d14ce9ce5c6f9601fddb
Kestutis Januskevicius <infohata@gmail.com>
2024-03-05 13:54:14 +0200

    PTU20 likes/pictures

--- ptu20_postit/postit_api/serializers.py
+++ ptu20_postit/postit_api/serializers.py
@@ -21,15 +21,37 @@ class PostSerializer(serializers.ModelSerializer):
     user_username = serializers.ReadOnlyField(source='user.username')
     comments = CommentSerializer(many=True, read_only=True)
     comment_count = serializers.SerializerMethodField()
+    like_count = serializers.SerializerMethodField()
 
     def get_comment_count(self, obj):
         return models.Comment.objects.filter(post=obj).count()
+    
+    def get_like_count(self, obj):
+        return models.PostLike.objects.filter(post=obj).count()
 
     class Meta:
         model = models.Post
         fields = [
-            'id', 'title', 'body', 
+            'id', 'title', 'body', 'image',
             'created_at', 'updated_at',
             'user_id', 'user_username',
-            'comment_count', 'comments',
+            'like_count', 'comment_count', 'comments',
         ]
+
+
+class PostLikeSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.id')
+    post = serializers.ReadOnlyField(source='post.id')
+
+    class Meta:
+        model = models.PostLike
+        fields = ['id', 'user', 'post']
+
+
+class CommentLikeSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.id')
+    comment = serializers.ReadOnlyField(source='comment.id')
+
+    class Meta:
+        model = models.CommentLike
+        fields = ['id', 'user', 'comment']
--- ptu20_postit/ptu20_postit/urls.py
+++ ptu20_postit/ptu20_postit/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('postit_api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
