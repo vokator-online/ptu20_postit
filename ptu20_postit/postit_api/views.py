0320c905e3e2f76c7d39b7a6eff46cfe0ed271ae
Kestutis Januskevicius <infohata@gmail.com>
2024-03-05 09:08:41 +0200

    comment CRUD

--- ptu20_postit/postit_api/views.py
+++ ptu20_postit/postit_api/views.py
@@ -35,3 +35,42 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
             return self.update(request, *args, **kwargs)
         else:
             raise ValidationError(_("You can only edit your own content."))
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        serializer.save(user=self.request.user, post=post)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        return queryset.filter(post=post)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        comment = models.Comment.objects.filter(
+            pk=kwargs['pk'], user=self.request.user
+        )
+        if comment.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(_("You can only delete your own content."))
+    
+    def put(self, request, *args, **kwargs):
+        comment = models.Comment.objects.filter(
+            pk=kwargs['pk'], user=self.request.user
+        )
+        if comment.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_("You can only edit your own content."))
--- ptu20_postit/ptu20_postit/settings.py
+++ ptu20_postit/ptu20_postit/settings.py
@@ -124,6 +124,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3
+}
+
 try:
     from . import local_settings
 except:
