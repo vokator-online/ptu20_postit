99d5ebda54b9045baf0b3a34277a3fbfd7970c88
Kestutis Januskevicius <infohata@gmail.com>
2024-03-05 08:29:21 +0200

    Post CRUD done

--- ptu20_postit/postit_api/views.py
+++ ptu20_postit/postit_api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from . import models, serializers
 
 
@@ -9,3 +11,27 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(
+            pk=kwargs['pk'], user=self.request.user
+        )
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(_("You can only delete your own content."))
+    
+    def put(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(
+            pk=kwargs['pk'], user=self.request.user
+        )
+        if post.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_("You can only edit your own content."))
