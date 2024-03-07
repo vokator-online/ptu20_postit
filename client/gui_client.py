398a3769b9e80c186db5bbe0f73d2f1e6f976b82
vokator <vokator@gmail.com>
2024-03-07 08:20:50 +0200

    Failo konfigūravimas

---
+++ client/gui_client.py
@@ -0,0 +1,18 @@
+import PySimpleGUI as sg
+
+sg.theme("dark")
+sg.set_options(font="sans-serif 20")
+
+main_layout = [
+    [
+        sg.Input("", key="-USERNAME-", size=15),
+        sg.Input("", key="-PASSWORD-", size=15 password_char=),
+        sg.Button("Login", key="-LLOGINOUT")
+
+
+    ]
+
+
+
+
+]
