837c94e156cd10f360b2846846be08ba4f028cce
Kestutis Januskevicius <infohata@gmail.com>
2024-03-13 09:04:10 +0200

    docker postgresql

--- ptu20_postit/ptu20_postit/settings.py
+++ ptu20_postit/ptu20_postit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,17 +72,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ptu20_postit.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'postgres',
+        'NAME': os.getenv('POSTGRES_DB', 'ptu20postit'),
+        'USER': os.getenv('POSTGRES_USER', 'ptu20'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
